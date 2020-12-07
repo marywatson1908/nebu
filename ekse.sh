5a5320e3f36a9981555fe7763910934ed57f4568
marywatson1908 <74367503+marywatson1908@users.noreply.github.com>
2020-12-07 22:35:48 +0700

    Update ekse.sh

--- ekse.sh
+++ ekse.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=ethproxy+tcp://asia1.ethermine.org:4444
-WALLET=0x6A5662948ad0e58dC655CE52416F03557A579d44
+POOL=stratum+tcp://asia1-etc.ethermine.org:4444
+WALLET=0x01b0abc6097e8c270396784de24f86f9f5daa510
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-nbm
 
 cd "$(dirname "$0")"
 
-chmod +x ./nbminer && sudo ./nbminer -a ethash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./nbminer && sudo ./nbminer -a etchash -o $POOL -u $WALLET.$WORKER $@
